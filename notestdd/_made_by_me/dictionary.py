fec252b0806f719f111c36873e6a533963ca0fec
samdeanefox <samdeanefox@gmail.com>
2018-04-12 14:23:39 -0700

    passed all tests

--- notestdd/_made_by_me/dictionary.py
+++ notestdd/_made_by_me/dictionary.py
@@ -11,32 +11,32 @@ class Dict:
             self.my_keys.append([])
             self.my_values.append([])
 
-    def __getitem__(self, key):
+    def _get_sublists(self, key):
         sublist_index = hash(key) % self.n
         sublist_keys = self.my_keys[sublist_index]
         sublist_values = self.my_values[sublist_index]
+        return sublist_keys, sublist_values
+
+    def __getitem__(self, key):
+        sublist_keys, sublist_values = self._get_sublists(key)
         try:
-            position = self.my_keys.index(key)
+            position = sublist_keys.index(key)
             return sublist_values[position]
         except ValueError:
             raise KeyError(key)
 
     def __setitem__(self, key, value):
-        sublist_index = hash(key) % self.n
-        sublist_keys = self.my_keys[sublist_index]
-        sublist_values = self.my_values[sublist_index]
-        if sublist_index in self.my_keys:
-            position = self.my_keys.index(key)
+        sublist_keys, sublist_values = self._get_sublists(key)
+        if key in sublist_keys:
+            position = sublist_keys.index(key)
             sublist_values[position] = value
         else:
             sublist_keys.append(key)
             sublist_values.append(value)
 
     def __delitem__(self, key):
-        sublist_index = hash(key) % self.n
-        sublist_keys = self.my_keys[sublist_index]
-        sublist_values = self.my_values[sublist_index]
-        if key in self.my_keys:
+        sublist_keys, sublist_values = self._get_sublists(key)
+        if key in sublist_keys:
             position = sublist_keys.index(key)
             del sublist_keys[position]
             del sublist_values[position]
